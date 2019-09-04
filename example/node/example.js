fc4fbfd29f2edd528b1f35d83b7d1be454ddd7d5
Dave Lucek <dlucek@gmail.com>
2019-09-04 16:02:57 -0500

    More openfeed buildout

--- example/node/example.js
+++ example/node/example.js
@@ -4,7 +4,8 @@ const version = require('./../../lib/index').version;
 
 const Connection = require('./../../lib/connection/websocket/Connection'),
 	SubscriptionType = require('./../../lib/connection/SubscriptionType'),
-	WebSocketAdapterFactoryForNode = require('./../../lib/connection/websocket/adapter/WebSocketAdapterFactoryForNode');
+	WebSocketAdapterFactoryForNode = require('./../../lib/connection/websocket/adapter/WebSocketAdapterFactoryForNode'),
+	OpenfeedCodec = require('./../../lib/connection/websocket/adapter/OpenfeedCodec');
 
 const CustomLoggingProvider = require('./logging/CustomLoggingProvider');
 
@@ -12,6 +13,7 @@ const LoggerFactory = require('./../../lib/logging/LoggerFactory');
 
 const ClientConfig = require('./../../lib/common/ClientConfig').ClientConfig,
 	Protocol = require('./../../lib/common/ClientConfig').Protocol;
+
 
 const startup = (() => {
 	'use strict';
@@ -46,60 +48,74 @@ const startup = (() => {
 	const password = process.argv[5];
 	const symbols = process.argv[6];
 
-	
+
 	var useOpenfeed = true;
 	var config = new ClientConfig();
-	var url = "ws://" +host + ":" + port + "/ws";
-	
-	if(useOpenfeed) {
+	var url = "ws://" + host + ":" + port + "/ws";
+
+	if (useOpenfeed) {
 		config.url = url;
 		config.protocol = Protocol.OPENFEED;
 		// Configurable protobuf path
 		// config.protoPath = "xxxx";
 	}
-
-	__logger.log(`Example: Instantiating Connection (using Node.js adapter) for [ ${username}/${password} ] @ [ ${url} ]`);
-
-	connection = new Connection(config);
-	adapterFactory = new WebSocketAdapterFactoryForNode(config);
-
-	connection.connect(host, username, password, adapterFactory);
-
-	// Handlers
-	connection.on(SubscriptionType.Timestamp, (message) => {
-		__logger.log('TS: ' + JSON.stringify(message));
-	});
-	connection.on(SubscriptionType.Events, (message) => {
-		__logger.log('Event: ' + JSON.stringify(message));
-	});
-
-	if (typeof symbols === 'string') {
-		symbols.split(',').forEach((s) => {
-			let price = null;
-
-			const handleMarketUpdate = (message) => {
-				switch (message.type) {
-					case "TOB":
-						__logger.log("< " + message.type +" "+ JSON.stringify(message));
-						break;
-					case "TRADE":
-						__logger.log("< " + message.type +" "+ JSON.stringify(message));
-						break;
-				}
-				let q = connection.getMarketState().getQuote(s);
-				const current = connection.getMarketState().getQuote(s).lastPrice;
-				if (price !== current) {
-					price = current;
-					__logger.log(`Example: ${s} = ${price}`);
-				}
-			};
-
-			const handleMarketDepth = (message) => {
-				__logger.log("< " + message.type + " " + JSON.stringify(message));
-			};
-
-			connection.on(SubscriptionType.MarketUpdate, handleMarketUpdate, s);
-			// connection.on(SubscriptionType.MarketDepth, handleMarketDepth, s);
-		});
+	var codec = null;
+	var startPromise = Promise.resolve();
+	if (config.isOpenfeed()) {
+		codec = new OpenfeedCodec(this._config);
+		startPromise = codec.init();
 	}
+
+	startPromise.then(() => {
+		__logger.log(`Example: Instantiating Connection (using Node.js adapter) for [ ${username}/${password} ] @ [ ${url} ]`);
+
+		connection = new Connection(config);
+		adapterFactory = new WebSocketAdapterFactoryForNode(config,codec);
+
+		connection.connect(host, username, password, adapterFactory);
+
+		// Handlers
+		connection.on(SubscriptionType.Timestamp, (message) => {
+			__logger.log('TS: ' + JSON.stringify(message));
+		});
+		connection.on(SubscriptionType.Events, (message) => {
+			__logger.log('Event: ' + JSON.stringify(message));
+		});
+
+		if (typeof symbols === 'string') {
+			symbols.split(',').forEach((s) => {
+				let price = null;
+
+				const handleMarketUpdate = (message) => {
+					switch (message.type) {
+						case "TOB":
+							// __logger.log("< " + message.type + " " + JSON.stringify(message));
+							break;
+						case "TRADE":
+							__logger.log("< " + message.type + " " + JSON.stringify(message));
+							break;
+					}
+					let q = connection.getMarketState().getQuote(s);
+					const current = connection.getMarketState().getQuote(s).lastPrice;
+					if (price !== current) {
+						price = current;
+						__logger.log(`Example: ${s} = ${price}`);
+					}
+				};
+
+				const handleMarketDepth = (message) => {
+					__logger.log("< " + message.type + " " + JSON.stringify(message));
+				};
+
+				const handleCumulativeVolume = (message) => {
+					__logger.log("< " + message.type + " " + JSON.stringify(message));
+				};
+
+				connection.on(SubscriptionType.MarketUpdate, handleMarketUpdate, s);
+				connection.on(SubscriptionType.CumulativeVolume, handleCumulativeVolume, s);
+			});
+		}
+	}).catch ( (err) => {
+		__logger.error("Start up issue: ",err);
+	})
 })();
