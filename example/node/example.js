333d34996eaac5115f0ef2c5808eb8582de683c7
Dave Lucek <dlucek@gmail.com>
2019-09-05 17:15:20 -0500

    added profile request, more openfeed buildout

--- example/node/example.js
+++ example/node/example.js
@@ -70,21 +70,29 @@ const startup = (() => {
 		__logger.log(`Example: Instantiating Connection (using Node.js adapter) for [ ${username}/${password} ] @ [ ${url} ]`);
 
 		connection = new Connection(config);
-		adapterFactory = new WebSocketAdapterFactoryForNode(config,codec);
+		adapterFactory = new WebSocketAdapterFactoryForNode(config, codec);
 
 		connection.connect(host, username, password, adapterFactory);
 
 		// Handlers
 		connection.on(SubscriptionType.Timestamp, (message) => {
-			__logger.log('TS: ' + JSON.stringify(message));
+			__logger.log('TS < ' + JSON.stringify(message));
 		});
 		connection.on(SubscriptionType.Events, (message) => {
-			__logger.log('Event: ' + JSON.stringify(message));
+			__logger.log('EVT < ' + JSON.stringify(message));
+			if (message.event === "login success") {
+			}
 		});
 
+
+		// Send subscriptions
 		if (typeof symbols === 'string') {
 			symbols.split(',').forEach((s) => {
 				let price = null;
+
+				const handleMarketDepth = (message) => {
+					__logger.log("< " + message.type + " " + JSON.stringify(message));
+				};
 
 				const handleMarketUpdate = (message) => {
 					switch (message.type) {
@@ -92,30 +100,29 @@ const startup = (() => {
 							// __logger.log("< " + message.type + " " + JSON.stringify(message));
 							break;
 						case "TRADE":
-							__logger.log("< " + message.type + " " + JSON.stringify(message));
+							// __logger.log("< " + message.type + " " + JSON.stringify(message));
 							break;
 					}
 					let q = connection.getMarketState().getQuote(s);
 					const current = connection.getMarketState().getQuote(s).lastPrice;
 					if (price !== current) {
 						price = current;
-						__logger.log(`Example: ${s} = ${price}`);
+						// __logger.log(`Example: ${s} = ${price}`);
 					}
 				};
 
-				const handleMarketDepth = (message) => {
-					__logger.log("< " + message.type + " " + JSON.stringify(message));
-				};
 
 				const handleCumulativeVolume = (message) => {
 					__logger.log("< " + message.type + " " + JSON.stringify(message));
 				};
 
+				// connection.on(SubscriptionType.MarketDepth, handleMarketDepth, s);
 				connection.on(SubscriptionType.MarketUpdate, handleMarketUpdate, s);
-				connection.on(SubscriptionType.CumulativeVolume, handleCumulativeVolume, s);
+				// connection.on(SubscriptionType.CumulativeVolume, handleCumulativeVolume, s);
 			});
 		}
-	}).catch ( (err) => {
-		__logger.error("Start up issue: ",err);
+
+	}).catch((err) => {
+		__logger.error("Start up issue: ", err);
 	})
 })();
