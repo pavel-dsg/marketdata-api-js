f034a6bcd945e86b2f0b15fc73fca5e3620577cb
Pavel Suchman <pavels@datascience.co.il>
2019-11-27 20:37:36 +0200

    Add ingest.js based on example.js

---
+++ example/node/ingest.js
@@ -0,0 +1,61 @@
+const process = require('process');
+const fs = require('fs');
+const version = require('./../../lib/meta').version;
+const Connection = require('./../../lib/connection/Connection'),
+	SubscriptionType = require('./../../lib/connection/SubscriptionType'),
+	WebSocketAdapterFactoryForNode = require('./../../lib/connection/adapter/WebSocketAdapterFactoryForNode');
+const CustomLoggingProvider = require('./logging/CustomLoggingProvider');
+const LoggerFactory = require('./../../lib/logging/LoggerFactory');
+const startup = (() => {
+	'use strict';
+	//LoggerFactory.configureForConsole();
+	//LoggerFactory.configureForSilence();
+	LoggerFactory.configure(new CustomLoggingProvider());
+	const __logger = LoggerFactory.getLogger('@barchart/example');
+	__logger.log(`Example: Node.js example script started [ version ${version} ]`);
+	let connection = null;
+	let adapterFactory = null;
+	process.on('SIGINT', () => {
+		__logger.log('\nExample: Processing SIGINT');
+		if (connection !== null) {
+			connection.disconnect();
+		}
+		__logger.log('Example: Node.js example script ending');
+		__logger.log('Closing file handler');
+		stream.end();
+		process.exit();
+	});
+	process.on('unhandledRejection', (error) => {
+		__logger.error('Unhandled Promise Rejection');
+		__logger.trace();
+	});
+	process.on('uncaughtException', (error) => {
+		__logger.error('Unhandled Error', error);
+		__logger.trace();
+	});
+	const host = process.argv[2];
+	const username = process.argv[3];
+	const password = process.argv[4];
+	const symbols = process.argv[5];
+	__logger.log(`Example: Instantiating Connection (using Node.js adapter) for [ ${username}/${password} ] @ [ ${host} ]`);
+	var stream = fs.createWriteStream("quote_log.txt", {flags:'a'});
+	connection = new Connection();
+	adapterFactory = new WebSocketAdapterFactoryForNode();
+	connection.connect(host, username, password, adapterFactory);
+	if (typeof symbols === 'string') {
+		symbols.split(',').forEach((s) => {
+			let price = null;
+			const handleMarketUpdate = function(message) {
+				const q = connection.getMarketState().getQuote(s);
+				const current = q.lastPrice;
+				if (price !== current) {
+					price = current;
+					__logger.log(`${s} = ${price}`);
+					//__logger.log(quote);
+					stream.write(`${Date()},${q.lastUpdate},${q.profile.root},${s},${q.profile.exchange},barchart,live,${q.bidPrice},${q.bidSize},${q.askPrice},${q.askSize},${q.lastPrice},${q.tradeSize}\n`);
+				}
+			};
+			connection.on(SubscriptionType.MarketUpdate, handleMarketUpdate, s);
+		});
+	}
+})();
